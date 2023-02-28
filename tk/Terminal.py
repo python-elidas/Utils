5e567b0ae0b9175e96b56fdbd6661f9ab69cd9b6
python-elidas <pyro.elidas@gmail.com>
2023-02-28 18:28:30 +0100

    Añadido scroll bar horizontal

--- tk/Terminal.py
+++ tk/Terminal.py
@@ -13,11 +13,11 @@ class TerminalWindow(Frame):
     '''
     A simplified way to create a scrollable window
     '''
-    def __init__(self, master):
+    def __init__(self, master, max_width):
         Frame.__init__(self, master)
                 
         self._master = master
-        self.__width = 40
+        self.__width, self.__max_width = 40, max_width
         #Creamos lo necesario
         self.__terminal = Listbox(
             self, width=self.__width, 
@@ -25,11 +25,14 @@ class TerminalWindow(Frame):
             font=('Consolas', 10)
         )
         self.__vbar = Scrollbar(self, orient=VERTICAL, command=self.__terminal.yview)
+        self.__hbar = Scrollbar(self, orient=HORIZONTAL, command=self.__terminal.xview)
         
         #Configuramos
-        self.__terminal.config(yscrollcommand=self.__vbar.set)
-        self.__terminal.pack(fill=BOTH, expand=1, side=LEFT)
+        self.__terminal.config(yscrollcommand=self.__vbar.set, xscrollcommand=self.__hbar.set)
         self.__vbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        self.__hbar.pack(fill=X, side=BOTTOM, expand=FALSE)
+        self.__terminal.pack(fill=BOTH, expand=0, side=TOP)
+        
         self.__set_width()
         
     def add(self, place, text):
@@ -37,15 +40,16 @@ class TerminalWindow(Frame):
     
     def __set_width(self):
         self.__terminal.update()
-        if self.__terminal.winfo_reqwidth() >= self.master.winfo_width():
-            while self.__terminal.winfo_reqwidth() >= self.master.winfo_width()-20:
+        if self.__terminal.winfo_reqwidth() >= self.__max_width:
+            while self.__terminal.winfo_reqwidth() >= self.__max_width-20:
                 self.__width -= 1
                 self.__terminal.config(width=self.__width)
-                print(self.__terminal.winfo_reqwidth(), self.master.winfo_width())
-        elif self.__terminal.winfo_reqwidth() < self.master.winfo_width():
-            while self.__terminal.winfo_reqwidth() <= self.master.winfo_width()-20:
+                #! print(self.__terminal.winfo_reqwidth(), self.__max_width)
+        elif self.__terminal.winfo_reqwidth() < self.__max_width:
+            while self.__terminal.winfo_reqwidth() <= self.__max_width-20:
                 self.__width += 1
                 self.__terminal.config(width=self.__width)
+                #! print(self.__terminal.winfo_reqwidth(), self.__max_width)
         
         
 #__usage Example__#
@@ -55,7 +59,8 @@ if __name__ == "__main__":
     root.geometry('400x250')
 
     #creation of the Terminal
-    terminal = TerminalWindow(root)
+    root.update()
+    terminal = TerminalWindow(root, root.winfo_width()-50)
     terminal.grid(row=0, column=0)
 
     #Adding some items
